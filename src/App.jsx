ee549bb15c265d3e032ef25e5415144e268400b9
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-17 12:18:25 +0100

    ADD: routes seturp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Header from "./components/Header";
 
 function App() {
 
   return (
-    <>
-      <Header />
+    <> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"/>
+          <Route path="movies/:id" />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
