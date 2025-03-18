38d2312bf904e8c81c35a62d7cf4f1fc06521a08
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-18 11:51:46 +0100

    ADD: fetch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
     return (
         <header>
-            <nav className="navbar navbar-expand-lg bg-light">
+            <nav className="navbar navbar-expand-lg bg-dark mb-3" data-bs-theme="dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="#">Movies</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { useState, useEffect } from "react";
+
 export default function Home() {
+    const [movies, setMovies] = useState([]);
+
+    const fetchMovies = () => {
+        fetch("http://localhost:3000/movies")
+            .then(res => res.json())
+            .then(data => setMovies(data.results))
+            .catch(err => console.error(err))
+    };
+
+    useEffect(fetchMovies, []);
+
     return (
-        <h1>Lista Film</h1>
+        <main className="container">
+            <h1>Lista Film</h1>
+        </main>
     )
 }
