cf073ef31af725f36ddf168451300557607b4743
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-18 12:50:58 +0100

    ADD: movie details page

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 export default function Movie() {
+    const {id} = useParams();
+    const [movie, setMovie] = useState({});
+
+    const fetchMovie = () => {
+        fetch(`http://localhost:3000/movies/${id}`)
+            .then(res => res.json())
+            .then(data => setMovie(data))
+    };
+
+    useEffect(fetchMovie, [])
+
     return (
-        <h1>Singolo film</h1>
+        <main className="container my-4">
+            <h1>{movie.title}</h1>
+            <div className="d-flex my-4 single-movie">
+                <figure className="w-25 py-3 pe-3">
+                    <img src={movie.image} alt={movie.title} className="w-100" />
+                </figure>
+                <div className="p-3">
+                    <h3>Details</h3>
+                    <div><strong>Director: </strong>{movie.director}</div>
+                    <div><strong>Genre: </strong>{movie.genre}</div>
+                    <div><strong>Release year: </strong>{movie.release_year}</div>
+                    <h3 className="mt-3">Abstract</h3>
+                    <p>{movie.abstract}</p>
+                </div>
+            </div>
+        </main>
     )
 }
