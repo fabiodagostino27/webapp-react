0234acbdad690c3c5fcff7f51d7605bc8ba1361e
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-18 13:05:40 +0100

    ADD: sticky navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
-        <header>
-            <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <header className="position-sticky top-0 z-1">
+            <nav className="navbar navbar-expand-lg bg-dark position-sticky top-0" data-bs-theme="dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Movies</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
